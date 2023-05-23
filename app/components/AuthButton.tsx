319c5bb5bb77581a92a607e1924bac98c0d73cd2
mncolla <maxincolla@gmail.com>
2023-05-23 11:36:02 -0300

    test: change replace to redirect

--- app/components/AuthButton.tsx
+++ app/components/AuthButton.tsx
@@ -5,7 +5,7 @@ const AuthButton = () => {
     "https://www.infojobs.net/api/oauth/user-authorize/index.xhtml?scope=MY_APPLICATIONS,CANDIDATE_PROFILE_WITH_EMAIL,CANDIDATE_READ_CURRICULUM_SKILLS,CV&client_id=0017c43570e04426b27e6bc8c75a846a&redirect_uri=https://infojobs-challenge.vercel.app&response_type=code";
 
   const handleClick = () => {
-    window.location.replace(URL);
+    window.location.href = URL;
   };
 
   return <button onClick={handleClick}>AuthButton</button>;
