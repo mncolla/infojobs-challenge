c196cb6bfed2898dc352370b6b9acea0fd8cb415
mncolla <maxincolla@gmail.com>
2023-05-23 11:24:10 -0300

    fix: bad url

--- app/components/AuthButton.tsx
+++ app/components/AuthButton.tsx
@@ -2,7 +2,7 @@
 
 const AuthButton = () => {
   const URL =
-    "https://www.infojobs.net/api/oauth/user-authorize/index.xhtml?scope=MY_APPLICATIONS,CANDIDATE_PROFILE_WITH_EMAIL,CANDIDATE_READ_CURRICULUM_SKILLS,CV&client_id=0017c43570e04426b27e6bc8c75a846a&redirect_uri=https://infojobs-challenge.vercel.app/&response_type=code";
+    "https://www.infojobs.net/api/oauth/user-authorize/index.xhtml?scope=MY_APPLICATIONS,CANDIDATE_PROFILE_WITH_EMAIL,CANDIDATE_READ_CURRICULUM_SKILLS,CV&client_id=0017c43570e04426b27e6bc8c75a846a&redirect_uri=https://infojobs-challenge.vercel.app&response_type=code";
 
   const handleClick = () => {
     window.location.replace(URL);
