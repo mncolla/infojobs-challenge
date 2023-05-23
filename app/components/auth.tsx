2fdbf36d2af5ed45a781fb8ad10a15c0ce90087a
mncolla <maxincolla@gmail.com>
2023-05-23 15:05:22 -0300

    feat: remove client_secret from front and connecto with api to improve flow and save access and refresh token

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -9,7 +9,6 @@ const NECESSARY_SCOPE = [
   "CANDIDATE_READ_CURRICULUM_FUTURE_JOB",
 ];
 const CLIENT_ID = "0017c43570e04426b27e6bc8c75a846a";
-const CLIENT_SECRET = "DNytAl3oFLlKvQR5EjrmtwXnwOb7q1MknBHI5vL7T8Fo2Mljyu";
 const REDIRECT_URI = "https://infojobs-challenge.vercel.app";
 
 const AuthButton = () => {
@@ -21,11 +20,19 @@ const AuthButton = () => {
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    const accessToken = urlParams.get("access_token");
+    const code = urlParams.get("code");
 
-    if (accessToken) {
-      document.cookie = `access_token=${accessToken}; path=/`;
-      window.location.href = "/";
+    if (code) {
+      fetch(`/api/authorize/${code}`, {
+        headers: {
+          "Content-Type": "application/json",
+          "InfoJobs-Code": code,
+        },
+      }).then(({ access_token, refresh_token, expires_in }: any) => {
+        document.cookie = `access_token=${access_token}; expires=${expires_in.toUTCString()}; path=/`;
+        document.cookie = `refresh_token=${refresh_token}; path=/`;
+      });
+      // window.location.href = "/";
     }
   }, []);
 
