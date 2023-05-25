11e6be75119b8e6894d804df84bc745a0c14fc9d
mncolla <maxincolla@gmail.com>
2023-05-25 04:34:16 -0300

    feat: styling navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Logo } from "./assets/logo";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -15,7 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} w-screen h-screen overflow-hidden`}>
+      <body
+        className={`${inter.className} w-screen h-screen overflow-hidden bg-[#f2f2f2] font-sans`}
+      >
+        <header className="bg-[#2088c2]">
+          <nav className="max-w-7xl mx-auto p-3 flex justify-center md:block">
+            <Logo color="white" width="100px" />
+          </nav>
+        </header>
         {children}
       </body>
     </html>
