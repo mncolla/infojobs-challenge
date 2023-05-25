038be851fb2d9a746a0858722948161d421597b7
mncolla <maxincolla@gmail.com>
2023-05-25 04:34:56 -0300

    feat: login layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,41 @@
 import { Logo } from "./assets/logo";
 import AuthButton from "./components/auth";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <main className="flex flex-col items-center w-full h-full mt-14">
-      <Logo />
-      <span>Generador de resume CV en texto</span>
+    <main className="flex w-full justify-center mt-14 ">
+      {/* <section className="flex flex-col bg-[#FFFFFF] w-[45%]">
+        <h2>Accede y mejora tu CV en texto</h2>
+
+        <label>Email</label>
+        <input
+          type="text"
+          disabled
+          className="bg-[#FFFFFF] border-solid rounded border h-[40px]"
+        />
+        <label>Email</label>
+        <input
+          type="text"
+          disabled
+          className="bg-[#FFFFFF] border-solid rounded border h-[40px]"
+        />
+      </section>
+      <section className="flex flex-col bg-[#FFFFFF] w-[45%]">
+        <h2>Accede y mejora tu CV en texto</h2>
+
+        <label>Email</label>
+        <input
+          type="text"
+          disabled
+          className="bg-[#FFFFFF] border-solid rounded border h-[40px]"
+        />
+        <label>Email</label>
+        <input
+          type="text"
+          disabled
+          className="bg-[#FFFFFF] border-solid rounded border h-[40px]"
+        />
+      </section> */}
       <AuthButton />
     </main>
   );
