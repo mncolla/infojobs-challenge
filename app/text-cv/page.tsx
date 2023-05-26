48dfd2ed8c357771dd6f2db2e497caeb89a748d6
mncolla <maxincolla@gmail.com>
2023-05-26 05:55:29 -0300

    chore: responsive designs

--- app/text-cv/page.tsx
+++ app/text-cv/page.tsx
@@ -3,8 +3,8 @@ import GenerateIAForm from "../components/generateForm";
 const TextCV = () => {
   return (
     <main className="w-full h-full flex justify-center">
-      <section className="bg-white w-[960px] mt-2 h-fit p-8">
-        <div className="box w-[645px] px-8 py-6 mx-auto">
+      <section className="bg-white w-full lg:w-[960px] mt-2 h-fit sm:p-8">
+        <div className="lg:w-[645px] px-4 sm:px-8 py-6 mx-auto">
           <GenerateIAForm />
         </div>
       </section>
