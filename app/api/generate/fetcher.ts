a339946252e3945920a27e93631b3e57b582ee5d
mncolla <maxincolla@gmail.com>
2023-05-26 05:49:15 -0300

    feat: optimize fetcher class

--- app/api/generate/fetcher.ts
+++ app/api/generate/fetcher.ts
@@ -4,20 +4,18 @@ export class Fetcher {
 
   constructor(baseApi: string, headers?: any) {
     this.fetchUrl = baseApi;
-    if (headers) {
-      this.headers = headers;
-    }
+    this.headers = {
+      "Content-Type": "application/json",
+      ...headers,
+    };
   }
 
   async get(url: string): Promise<any> {
-    const headers = {
-      method: "GET",
-      "Content-Type": "application/json",
-      ...this.headers,
-    };
-
     const res = await fetch(`${this.fetchUrl}${url}`, {
-      headers,
+      headers: {
+        method: "GET",
+        ...this.headers,
+      },
     });
 
     const data = await res.json();
