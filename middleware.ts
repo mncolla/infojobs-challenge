606d2d59c44460e42c3608d102445e30ccf9cc5e
mncolla <maxincolla@gmail.com>
2023-05-26 07:47:06 -0300

    fix: delete basic token

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  /* let basicCookie = request.cookies.get("basic_token")?.value;
-  let accessCookie = request.cookies.get("access_token")?.value; */
-
-  if (
-    !request.cookies.has("basic_token") &&
-    !request.cookies.has("access_token")
-  ) {
+  if (!request.cookies.has("access_token")) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 }
