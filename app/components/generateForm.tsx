7066fac839997aa8527c460f931bd119bf23477f
mncolla <maxincolla@gmail.com>
2023-05-26 05:55:16 -0300

    feat: responsive design mistakes, implement api call and show response in textarea

--- app/components/generateForm.tsx
+++ app/components/generateForm.tsx
@@ -1,11 +1,35 @@
 "use client";
 
+import { FormEvent, TextareaHTMLAttributes, useRef, useState } from "react";
+
 const GenerateIAForm = () => {
-  const handleSubmit = () => {
-    console.log("algoo");
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+
+  const textCvRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => e.preventDefault();
+
+  const handleGenerateIA = () => {
+    setIsFetching(true);
+    fetch(`/api/generate`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then(({ text }: any) => {
+        console.log("text", text);
+        textCvRef.current!.value = text;
+        setIsFetching(false);
+      });
   };
+
   return (
-    <form action="" onSubmit={handleSubmit} className="w-[446px] mx-auto">
+    <form
+      action=""
+      onSubmit={handleSubmit}
+      className="w-full md:w-[446px] mx-auto"
+    >
       <h1 className="text-center  text-3xl font-bold text-[#2d3133] mb-9">
         Editar CV en texto
       </h1>
@@ -16,22 +40,31 @@ const GenerateIAForm = () => {
         className="resize-y w-full appearance-none min-h-[150px] mx-auto overflow-hidden border rounded border-[#c7c7c7] px-2 py-1 focus:outline-none focus:border-[#167db7] focus:border focus:border-solid"
         name="textCvData"
         id="textCvData"
+        ref={textCvRef}
+        disabled={isFetching}
         placeholder="Completa tu perfil con información adicional relevante para las empresas o datos que no hayas podido introducir en otros campos."
       ></textarea>
-      <div className="h-[80px] flex items-center text-sm transition-all">
-        <button className="bg-[#167db7] border-[#167db7] border border-solid rounded text-white shadow-lg px-4 py-2 font-semibold transition-all">
+      <div className="h-[80px] flex  items-center text-xs sm:text-sm transition-all">
+        <button
+          disabled={isFetching}
+          className="bg-[#167db7] border-[#167db7] border border-solid rounded text-white shadow-lg px-4 py-2 font-semibold transition-all"
+        >
           GUARDAR
         </button>
-        <button className="hover:bg-[#167db7] border-[#167db7] border border-solid rounded hover:text-white text-[#167db7] shadow-lg px-4 py-2 font-semibold ml-2 transition-all">
+        <button
+          disabled={isFetching}
+          className="hover:bg-[#167db7] border-[#167db7] border border-solid rounded hover:text-white text-[#167db7] shadow-lg px-4 py-2 font-semibold mx-2 transition-all"
+        >
           CANCELAR
         </button>
         <div className="w-full flex justify-end">
-          <button className="bg-[#2D3133] border-[#2D3133] border border-solid rounded text-white shadow-lg px-4 py-2 font-semibold transition-all ">
-            GENERAR CON IA
+          <button
+            onClick={handleGenerateIA}
+            className="bg-[#2D3133] sm:flex border-[#2D3133] border border-solid rounded text-white shadow-lg px-4 py-2 font-semibold transition-all "
+          >
+            <span className="hidden sm:block">GENERAR CON &nbsp;</span>
+            <span>IA</span>
           </button>
-          {/* <button className="bg-[#2D3133] border-[#2D3133] border border-solid rounded text-white shadow-lg px-2 py-2 font-semibold transition-all ">
-                  <SettingsIcon color="white" />
-                </button >*/}
         </div>
       </div>
     </form>
