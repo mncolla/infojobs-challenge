71e0fa45f520596c10f6480a1a227ff07f7b6ea8
mncolla <maxincolla@gmail.com>
2023-05-27 05:53:22 -0300

    test: move read token and auth to auth button (not final)

--- app/login/components/auth-button.tsx
+++ app/login/components/auth-button.tsx
@@ -1,14 +1,42 @@
 "use client";
 
+import { useEffect } from "react";
+
 const necesaryScope =
   "CV,CANDIDATE_READ_CURRICULUM_EDUCATION,CANDIDATE_READ_CURRICULUM_EXPERIENCE,CANDIDATE_READ_CURRICULUM_FUTURE_JOB,CANDIDATE_EDIT_CURRICULUM_CVTEXT,CANDIDATE_READ_CURRICULUM_CVTEXT";
 const clientId = "0017c43570e04426b27e6bc8c75a846a";
-const redirectUri = "https://infojobs-challenge.vercel.app";
+const redirectUri = "https://infojobs-challenge.vercel.app/login";
 
 const AuthButton = () => {
   const handleLogin = () => {
     window.location.href = `https://www.infojobs.net/api/oauth/user-authorize/index.xhtml?scope=${necesaryScope}&client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`;
   };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+
+    if (code) {
+      fetch(`/api/authorize`, {
+        headers: {
+          "Content-Type": "application/json",
+          "InfoJobs-Code": code,
+        },
+      })
+        .then((data) => data.json())
+        .then(({ data: { access_token, expires_in } }) => {
+          const expirationDate = new Date(
+            Date.now() + expires_in * 1000
+          ).toUTCString();
+          document.cookie = `access_token=${access_token}; expires=${expirationDate}; path=/`;
+          window.history.pushState(
+            {},
+            document.title,
+            window.location.pathname
+          );
+        });
+    }
+  }, []);
 
   return (
     <button
--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,11 @@
 import GenerateIAForm from "./components/form";
 
-const getAccessToken = async (code: string) => {
-  const data = await fetch(`/api/authorize`, {
-    headers: {
-      "Content-Type": "application/json",
-      "InfoJobs-Code": code,
-    },
-  });
-  const { access_token, expires_in }: any = data.json();
-  const expirationDate = new Date(Date.now() + expires_in * 1000).toUTCString();
-
-  return { accessToken: access_token, expirationDate };
-};
-
-/* const getTextCvData = async () => {
-  const data = await fetch(`/api/textcv`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  const { textCv }: any = data.json();
-
-  return textCv;
-}; */
-
 const TextCV = async ({ searchParams: { code } }: any) => {
-  if (code) {
-    const { accessToken, expirationDate }: any = await getAccessToken(code);
-    document.cookie = `access_token=${accessToken}; expires=${expirationDate}; path=/`;
-    window.history.pushState({}, document.title, window.location.pathname);
-  }
-
-  const textCv = ""; /* await getTextCvData(); */
-
   return (
     <main className="w-full h-full flex justify-center">
       <section className="bg-white w-full lg:w-[960px] mt-2 h-fit sm:p-8">
         <div className="lg:w-[645px] px-4 sm:px-8 py-6 mx-auto">
-          <GenerateIAForm data={textCv} />
+          <GenerateIAForm />
         </div>
       </section>
     </main>
