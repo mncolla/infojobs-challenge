b56a5f7dc870f8662d1207d9463ab773fc716c58
mncolla <maxincolla@gmail.com>
2023-05-27 04:39:35 -0300

    feat: change guards on middleware

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,19 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (!request.cookies.has("access_token")) {
+  const hasCode = request.nextUrl.searchParams.has("code");
+  const hasCookie = request.cookies.has("access_token");
+  const isLoginURL = request.nextUrl.pathname.startsWith("/login");
+
+  if (!hasCode && !hasCookie && !isLoginURL) {
     return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (isLoginURL && hasCookie) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
 }
 
 export const config = {
-  matcher: "/",
+  matcher: ["/", "/login"],
 };
