4ab0b32792237c12e662744095f1e6b71579c6d8
mncolla <maxincolla@gmail.com>
2023-05-27 07:16:20 -0300

    feat: put textcv

--- app/api/textcv/route.ts
+++ app/api/textcv/route.ts
@@ -7,14 +7,7 @@ const appToken = process.env.APP_TOKEN;
 
 export async function GET(request: Request) {
   const cookieStore = cookies();
-
-  const accessToken: any = cookieStore.get("access_token")?.value;
-
-  if (!accessToken) {
-    return new Response(JSON.stringify({ error: "Not authorized" }), {
-      status: 401,
-    });
-  }
+  const accessToken = cookieStore.get("access_token")?.value || null;
 
   const headers = {
     Authorization: `Basic ${appToken}, Bearer ${accessToken}`,
@@ -22,11 +15,26 @@ export async function GET(request: Request) {
   const fetcher = new Fetcher(BASE_URL_API, headers);
   const [curriculum]: Curriculum[] = await fetcher.get("/2/curriculum");
   const curriculumId = curriculum.code;
-  const textCv = await fetcher.get(`/1/curriculum/${curriculumId}/cvtext`);
+  const { cvtext } = await fetcher.get(`/1/curriculum/${curriculumId}/cvtext`);
 
-  console.log(textCv);
-
-  return new Response(JSON.stringify({ textCv }), { status: 200 });
+  return new Response(JSON.stringify({ data: cvtext }), { status: 200 });
 }
 
-export async function PUT(request: Request) {}
+export async function PUT(request: Request) {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get("access_token")?.value || null;
+
+  const headers = {
+    Authorization: `Basic ${appToken}, Bearer ${accessToken}`,
+  };
+  const fetcher = new Fetcher(BASE_URL_API, headers);
+  const [curriculum]: Curriculum[] = await fetcher.get("/2/curriculum");
+  const curriculumId = curriculum.code;
+  const cvtext = await request.json();
+
+  const response = await fetcher.put(`/1/curriculum/${curriculumId}/cvtext`, {
+    cvtext,
+  });
+
+  return new Response(JSON.stringify({ data: response }), { status: 200 });
+}
