5f0f7240fc5622b91bce58d1dbea7fb75c9033c5
mncolla <maxincolla@gmail.com>
2023-05-27 05:57:47 -0300

    fix: middlewaqre matcher

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (request.cookies.has("access_token")) {
+  if (!request.cookies.has("access_token")) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 }
 
 export const config = {
-  matcher: ["/"],
+  matcher: "/",
 };
