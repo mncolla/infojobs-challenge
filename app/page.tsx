7a2a0d524a8b71bfdc5d2b7f269f06647a3cd1ef
mncolla <maxincolla@gmail.com>
2023-05-27 04:39:57 -0300

    feat: remove code read from button to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
 import GenerateIAForm from "./components/form";
-
-/* const getUserCVText = async () => {
-  const res = await fetch('/')
-} */
+import { redirect } from "next/navigation";
 
 const TextCV = async () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get("code");
+
+  if (code) {
+    const data = await fetch(`/api/authorize`, {
+      headers: {
+        "Content-Type": "application/json",
+        "InfoJobs-Code": code,
+      },
+    });
+    const { access_token, expires_in }: any = data.json();
+
+    const expirationDate = new Date(
+      Date.now() + expires_in * 1000
+    ).toUTCString();
+    document.cookie = `access_token=${access_token}; expires=${expirationDate}; path=/`;
+    window.history.pushState({}, document.title, window.location.pathname);
+  }
+
   return (
     <main className="w-full h-full flex justify-center">
       <section className="bg-white w-full lg:w-[960px] mt-2 h-fit sm:p-8">
