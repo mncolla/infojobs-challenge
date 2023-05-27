f7476caa36c05acb11c02a7fafb251c53c4ce8b9
mncolla <maxincolla@gmail.com>
2023-05-27 07:17:52 -0300

    feat: handle api error in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (!request.cookies.has("access_token")) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  const pathname = request.nextUrl.pathname;
+
+  if (
+    (pathname.startsWith("/api/textcv") ||
+      pathname.startsWith("/api/generate")) &&
+    !request.cookies.has("access_token")
+  ) {
+    return new NextResponse(
+      JSON.stringify({ success: false, message: "authentication failed" }),
+      { status: 401, headers: { "content-type": "application/json" } }
+    );
   }
 }
 
 export const config = {
-  matcher: "/",
+  matcher: ["/", "/api/textcv", "/api/generate"],
 };
