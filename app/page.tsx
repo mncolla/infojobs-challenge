ddd97912b25b65add84dd10f65050f19aa683d6b
mncolla <maxincolla@gmail.com>
2023-05-28 06:15:38 -0300

    fix: scroll

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import GenerateIAForm from "./components/form";
 
 const TextCV = async ({ searchParams: { code } }: any) => {
   return (
-    <main className="w-full h-full flex justify-center">
+    <main className="w-full flex justify-center">
       <section className="bg-white w-full lg:w-[960px] mt-2 h-fit sm:p-8">
         <div className="lg:w-[645px] px-4 sm:px-8 py-6 mx-auto">
           <GenerateIAForm />
